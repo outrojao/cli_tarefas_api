acff6db4c0966dfade8345c2da8775f1d3ed771a
João Victor <joaovictordsantosr@gmail.com>
2025-11-18 18:42:22 -0300

    Ensure task name is unique in tasks table creation

--- internal/database/migrations/20251112161523_create_tasks_table.sql
+++ internal/database/migrations/20251112161523_create_tasks_table.sql
@@ -2,7 +2,7 @@
 -- +goose StatementBegin
 create table if not exists tasks (
    id         serial primary key,
-   name       varchar(255) not null,
+   name       varchar(255) unique not null,
    done       boolean not null default false,
    created_at timestamp with time zone not null default current_timestamp
 );
